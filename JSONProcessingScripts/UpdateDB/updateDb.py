0d54831e6866548f278933defacc978934660122
CJflamebolt <cjflamebolt@gmail.com>
2025-04-28 23:12:22 -0500

    we got update db working sheeeeeesh

--- JSONProcessingScripts/UpdateDB/updateDb.py
+++ JSONProcessingScripts/UpdateDB/updateDb.py
@@ -3,8 +3,7 @@ import json
 # Open the JSON file and load its content
 #with open('input.json', 'r') as file:
 #    input = json.load(file)
-# with open('input.json', 'r') as file:
-#     input = json.load(file)
+
 
 # Open db json file and update
 with open('db.json', 'r') as file:
@@ -19,8 +18,28 @@ with open('db.json', 'r') as file:
 #     print(player)
 #     print(player.name)
 
-print(db['PAULO'])
+with open('input.json', 'r') as file:
+    input = json.load(file)
+
+
+
+newPlayers = [] # List of new players not in db
+for playerInput in input:
+    inDB = False
+    for playerDB in db:
+        if (playerInput['name'] == playerDB['name']):   # in the future check by id instead of name
+            playerDB['swissWins'] = playerDB['swissWins'] + playerInput['swissWins'] # Add swissWins and points to db listing
+            playerDB['points'] = playerDB['points'] + playerInput['points']
+            inDB = True
+    if (inDB == False):
+        newPlayers.append(playerInput)
+
+with open('playersToAdd.json', 'w') as file:
+    json.dump(newPlayers, file, indent=4)  # 'indent' makes the output more readable
+
+
+
 # Write JSON input to a file
-#with open('db.json', 'w') as file:
-#   json.dump(db, file, indent=4)  # 'indent' makes the output more readable
+with open('newDB.json', 'w') as file:
+   json.dump(db, file, indent=4)  # 'indent' makes the output more readable
 
