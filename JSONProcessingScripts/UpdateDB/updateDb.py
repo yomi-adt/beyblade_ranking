59e275ca47e9070d79a8a1ddbae5227f819f7bf4
CJflamebolt <cjflamebolt@gmail.com>
2025-04-28 22:20:24 -0500

    commented a buncha stuff

--- JSONProcessingScripts/UpdateDB/updateDb.py
+++ JSONProcessingScripts/UpdateDB/updateDb.py
@@ -1,17 +1,19 @@
 import json
 
 # Open the JSON file and load its content
-with open('input.json', 'r') as file:
-    input = json.load(file)
+#with open('input.json', 'r') as file:
+#    input = json.load(file)
 
 # Open db json file and update
 with open('db.json', 'r') as file:
     db = json.load(file)
 
 
-for player in input:
-    print(player)
-    print(player.name)
+#for player in db:
+    #print(player)
+
+##print(db[1]['swissWins'] += d)
 # Write JSON input to a file
-with open('db.json', 'w') as file:
-    json.dump(db, file, indent=4)  # 'indent' makes the output more readable
+#with open('db.json', 'w') as file:
+#   json.dump(db, file, indent=4)  # 'indent' makes the output more readable
+
