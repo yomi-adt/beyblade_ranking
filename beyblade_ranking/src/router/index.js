ac4cfe6554d5d2a168e99d042acc1c41e04b4c21
yomi-adt <150170628+yomi-adt@users.noreply.github.com>
2025-05-06 20:24:50 -0500

    organizer step 3

--- beyblade_ranking/src/router/index.js
+++ beyblade_ranking/src/router/index.js
@@ -3,6 +3,7 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 // Import your views
 import Home from '../views/Home.vue'
 import Organizer from '../views/Organizer.vue'
+import Organizer3 from '../views/Organizer3.vue'
 
 // Create and export router
 const router = createRouter({
@@ -17,6 +18,11 @@ const router = createRouter({
       path: '/organizer', // Todo: Set path to some random UUID string so that it's harder to access
       name: 'Organizer',
       component: Organizer,
+    },
+    {
+      path: '/organizer3', // Todo: Set path to some random UUID string so that it's harder to access
+      name: 'Organizer3',
+      component: Organizer3,
     },
   ]
 })
---
+++ beyblade_ranking/src/views/Organizer3.vue
@@ -0,0 +1,7 @@
+<script setup>
+    import OrganizerTable from '../components/OrganizerTable.vue';
+</script>
+
+<template>
+    <OrganizerTable />
+</template>
