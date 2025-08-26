b45a64441288cfb7d70295502fa748da057f1027
yomi-adt <nbarlaab@gmail.com>
2025-08-26 13:50:59 -0500

    New players

--- beyblade_ranking/src/App.vue
+++ beyblade_ranking/src/App.vue
@@ -19,7 +19,7 @@ import { Button } from 'primevue';
       <RouterView />
     </div>
 
-    <p style="font-style: italic">Last updated July 9th, 2025</p>
+    <p style="font-style: italic">Last updated August 26, 2025</p>
   </div>
 </template>
 
