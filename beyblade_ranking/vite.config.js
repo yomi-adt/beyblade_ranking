2d27c3c490711d11b9ac21b3e45df3012932beee
yomi-adt <nbarlaab@gmail.com>
2026-07-19 17:56:29 -0500

    testing auto deploy change

--- beyblade_ranking/vite.config.js
+++ beyblade_ranking/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vite.dev/config/ test
 export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production' ? '/beyblade_ranking/' : '/',
